92f4dcc11dafb1a1b2dbaada9eee08ac6e653921
David Lee <dhlee@usc.edu>
2022-03-06 16:09:43 -0800

    use vscode snippet for tex

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -5,7 +5,6 @@ M.config = function()
   -- Snippets
   -- =========================================
   require("luasnip/loaders/from_vscode").load { paths = { "~/.config/lvim/snippets" } }
-  require("user.snippets")
 
   -- Autopairs
   -- =========================================
